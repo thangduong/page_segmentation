e06e054112b7dee5884dca16653f77ac1e09280a
Thang Duong <thang@quantee.com>
2018-06-11 14:47:27 -0700

    changed Linux to linux

--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
- bin/Linux64/segment -input_file test_data/evorus-0000.png -output_dir test_data/ -vp1_min_break_size 0 -hp0_min_break_size 20
+ bin/linux64/segment -input_file test_data/evorus-0000.png -output_dir test_data/ -vp1_min_break_size 0 -hp0_min_break_size 20
